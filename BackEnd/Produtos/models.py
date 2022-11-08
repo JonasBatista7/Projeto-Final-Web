6beaae6148bc9ec983d707e15cd6a74d73a77c32
Jonas <jonas.batiista1@gmail.com>
2022-11-08 20:14:15 -0300

    Finalização BackEnd

--- BackEnd/Produtos/models.py
+++ BackEnd/Produtos/models.py
@@ -10,17 +10,23 @@ class Produto(models.Model):
     DESCRICAO = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.name
+        return self.NOME_PRODUTO
 
+TIPO_STATUS = [
+    ('Pedido Realizado', 'Pedido Realizado'),
+    ('Pedido Confirmado', 'Pedido Confirmado'),
+]
 
 class Carrinho(models.Model):
+    NOME_CARRINHO = models.CharField(max_length=30)
     ID_PRODUTO = models.ForeignKey(Produto, on_delete=models.DO_NOTHING)
     QUANTIDADE = models.IntegerField()
     ID_CLIENTE = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    STATUS_CARRINHO = models.CharField(
+        max_length=40,
+        choices=TIPO_STATUS,
+        default='Pedido Realizado',
+    )
 
     def __str__(self):
-        return self.name
-
-
-class Pedidos(models.Model):
-    ID_CARRINHO: models.ForeignKey(Carrinho, on_delete=models.DO_NOTHING)
+        return self.NOME_CARRINHO
--- BackEnd/backend/urls.py
+++ BackEnd/backend/urls.py
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from .views import UserViewSet, GroupViewSet
+from Produtos.views import ProdutoViewSet, CarrinhoViewSet
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'groups', GroupViewSet)
+router.register(r'Produto', ProdutoViewSet)
+router.register(r'Carrinho', CarrinhoViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
