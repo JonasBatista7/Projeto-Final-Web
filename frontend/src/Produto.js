91f04ebbf673da07c3fabaebca3bcbabd72a0dc6
root <root@DESKTOP-E25VOVL>
2022-11-09 23:00:05 -0300

    Consumindo dados do back e visualizando no front

--- frontend/src/Produto.js
+++ frontend/src/Produto.js
@@ -1,5 +1,11 @@
 import React from 'react'
 
 export default function Produto(props) {
-    return <li>Desc.: { props.name } </li>
+    return <li> 
+        <p>{ props.NOME_PRODUTO }</p>
+        <p>{ props.CREATED_BY }</p>
+        <p>{ props.VALOR }</p>
+        <p>{ props.QUANTIDADE_ESTOQUE }</p>
+        <p>{ props.DESCRICAO}</p>
+</li>
 }
--- frontend/src/Produtos.js
+++ frontend/src/Produtos.js
@@ -1,15 +1,31 @@
 import React from 'react'
 import Produto from './Produto'
 
-export default function Produtos() {
-    return (
-        <div>
-            <h2>Produtos</h2>
-            <ul>
-            <Produto name={"Meu Produto"}/>
-            <Produto name={"Meu Produto 2"}/>
 
-            </ul>
-        </div>
-    )
+export default class  Produtos extends React.Component{
+    state = {lists: [], loading:true}
+    async componentDidMount(){
+        const config = {
+            headers : {
+                'Content-Type': 'application/json',
+                'Authorization': 'Token cf0517dd3818eb8a18e5e3abdd4db0d686de0c89'
+            }
+        }
+        var url = 'http://127.0.0.1:8000/Produto/'
+        const response = await fetch(url, config);
+        const data = await response.json();
+        console.log(data);
+        this.setState({lists:data, loading:false});
+    }
+    render()
+    {
+        const listApi = this.state.lists;
+        return (
+            <div>
+                <h2>Produtos</h2>
+                 {listApi.map(list => <Produto key={list.id} NOME_PRODUTO={list.NOME_PRODUTO} CREATED_BY={list.CREATED_BY} VALOR={list.VALOR}/>)}
+
+            </div>
+        )
+    }
 }
